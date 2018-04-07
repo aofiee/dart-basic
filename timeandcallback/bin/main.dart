f59777793b425da0a07be8b301c19ce6d5e31ff8
Khomkrid Lerdprasert <khomkrid@twinsynergy.co.th>
2018-04-07 17:08:28 +0700

    ok

---
+++ timeandcallback/bin/main.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+int counter = 0;
+main(List<String> arguments) {
+  Duration duration = new Duration(seconds: 1);
+  Timer timer = new Timer.periodic(duration, timeout);
+  print("Started ${getTime()}");
+}
+
+void timeout(Timer timer){
+  print("Timeout ${getTime()}");
+  counter++;
+  if(counter >= 5) timer.cancel();
+}
+
+Stream getTime(){
+  DateTime dt = new DateTime.now();
+  return dt.toString();
+}
