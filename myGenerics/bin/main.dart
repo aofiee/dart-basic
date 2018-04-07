057ee901e32736773e03334caa0c957f169bb78a
Khomkrid Lerdprasert <khomkrid@twinsynergy.co.th>
2018-04-07 11:37:37 +0700

    ok

--- myGenerics/bin/main.dart
+++ myGenerics/bin/main.dart
@@ -1,5 +1,19 @@
 import 'package:myGenerics/myGenerics.dart' as myGenerics;
+import 'package:myGenerics/counter.dart';
 
 main(List<String> arguments) {
-  print('Hello world: ${myGenerics.calculate()}!');
+  List values = [1,1,1,1,1,1];
+  print(add(1,values));
+
+  Counter<double> d = new Counter<double>();
+  d.addAll([1.0,2.0,3.0]);
+  d.total();
+}
+
+T add<T extends num>(T value,List<T> items){
+  T ret = value;
+  items.forEach((value){
+    ret = ret + value;
+  });
+  return ret;
 }
---
+++ myGenerics/lib/counter.dart
@@ -0,0 +1,13 @@
+class Counter <T extends num> {
+  List<T> _items = new List<T>();
+  void addAll(Iterable<T> iterable) => _items.addAll(iterable);
+  void add(T value) => _items.add(value);
+  T emlementAt(int index) => _items.elementAt(index);
+  void total(){
+    T value = 0.0;
+    _items.forEach((item){
+       value = value+item;
+    });
+    print(value);
+  }
+}
